4ceedb6490535f7eaca67c53b1639f5f3b8a524f
Deyby <deibyruiz2@gmail.com>
2026-06-06 18:03:31 -0500

    chore: limpieza total de estructura y aplicacion de gitignore definitivo

--- src/main/java/com/torneo/goldesk/GoldeskApplication.java
+++ src/main/java/com/torneo/goldesk/GoldeskApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class GoldeskApplication {
 
 	public static void main(String[] args) {
+		// Carga el archivo .env y lo inyecta en las variables de sistema
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(GoldeskApplication.class, args);
 
 	}
