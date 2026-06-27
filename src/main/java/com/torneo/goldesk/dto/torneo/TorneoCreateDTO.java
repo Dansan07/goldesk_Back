6e143c4fa274c9722ad1cb9f0f44ec88eaee1865
Deyby <deibyruiz2@gmail.com>
2026-06-27 11:11:10 -0500

    se crea clase padre para torneo y clases hijas que lo heredan para gestionar su información.

--- src/main/java/com/torneo/goldesk/dto/torneo/TorneoCreateDTO.java
+++ src/main/java/com/torneo/goldesk/dto/torneo/TorneoCreateDTO.java
@@ -1,55 +1,8 @@
 package com.torneo.goldesk.dto.torneo;
 
-public class TorneoCreateDTO {
+public class TorneoCreateDTO extends TorneoDTO {
 
-    private String cedulaOrganizador;
-    private String nombreTorneo;
-    private double valorAmarilla;
-    private double valorAzul;
-    private double valorRoja;
-    private double valorArbitraje;
-    private double valorInscripcion;
-    private double valorBalonPetos;
-    private Integer partidosInicial;
-    private String categoriaTorneo;
-
-    public String getCategoriaTorneo() {
-        return categoriaTorneo;
-    }
-
-    public String getCedulaOrganizador() {
-        return cedulaOrganizador;
-    }
-
-    public String getNombreTorneo() {
-        return nombreTorneo;
-    }
-
-    public double getValorAmarilla() {
-        return valorAmarilla;
-    }
-
-    public double getValorAzul() {
-        return valorAzul;
-    }
-
-    public double getValorRoja() {
-        return valorRoja;
-    }
-
-    public double getValorArbitraje() {
-        return valorArbitraje;
-    }
-
-    public double getValorInscripcion() {
-        return valorInscripcion;
-    }
-
-    public double getValorBalonPetos() {
-        return valorBalonPetos;
-    }
-
-    public Integer getPartidosInicial() {
-        return partidosInicial;
+    public TorneoCreateDTO(String cedulaOrganizador, String nombreTorneo, double valorAmarilla, double valorAzul, double valorRoja, double valorArbitraje, double valorInscripcion, double valorBalonPetos, Integer partidosInicial, String categoriaTorneo, Boolean activo) {
+        super(cedulaOrganizador, nombreTorneo, valorAmarilla, valorAzul, valorRoja, valorArbitraje, valorInscripcion, valorBalonPetos, partidosInicial, categoriaTorneo, activo);
     }
 }
--- src/main/java/com/torneo/goldesk/dto/torneo/TorneoResponseDTO.java
+++ src/main/java/com/torneo/goldesk/dto/torneo/TorneoResponseDTO.java
@@ -1,80 +1,8 @@
 package com.torneo.goldesk.dto.torneo;
 
-public class TorneoResponseDTO {
-
-    private Integer idTorneo;
-    private String cedulaOrganizador;
-    private String nombreTorneo;
-    private double valorAmarilla;
-    private double valorAzul;
-    private double valorRoja;
-    private double valorArbitraje;
-    private double valorInscripcion;
-    private double valorBalonPetos;
-    private Integer partidosInicial;
-    private String categoriaTorneo;
-    private Boolean activo;
+public class TorneoResponseDTO extends TorneoDTO{
 
     public TorneoResponseDTO(Integer idTorneo, String cedulaOrganizador, String nombreTorneo, double valorAmarilla, double valorAzul, double valorRoja, double valorArbitraje, double valorInscripcion, double valorBalonPetos, Integer partidosInicial, String categoriaTorneo, Boolean activo) {
-        this.idTorneo = idTorneo;
-        this.cedulaOrganizador = cedulaOrganizador;
-        this.nombreTorneo = nombreTorneo;
-        this.valorAmarilla = valorAmarilla;
-        this.valorAzul = valorAzul;
-        this.valorRoja = valorRoja;
-        this.valorArbitraje = valorArbitraje;
-        this.valorInscripcion = valorInscripcion;
-        this.valorBalonPetos = valorBalonPetos;
-        this.partidosInicial = partidosInicial;
-        this.categoriaTorneo = categoriaTorneo;
-        this.activo = activo;
-    }
-
-    public String getCategoriaTorneo() {
-        return categoriaTorneo;
-    }
-
-    public Integer getIdTorneo() {
-        return idTorneo;
-    }
-
-    public String getCedulaOrganizador() {
-        return cedulaOrganizador;
-    }
-
-    public String getNombreTorneo() {
-        return nombreTorneo;
-    }
-
-    public double getValorAmarilla() {
-        return valorAmarilla;
-    }
-
-    public double getValorAzul() {
-        return valorAzul;
-    }
-
-    public double getValorRoja() {
-        return valorRoja;
-    }
-
-    public double getValorArbitraje() {
-        return valorArbitraje;
-    }
-
-    public double getValorInscripcion() {
-        return valorInscripcion;
-    }
-
-    public double getValorBalonPetos() {
-        return valorBalonPetos;
-    }
-
-    public Integer getPartidosInicial() {
-        return partidosInicial;
-    }
-
-    public boolean getActivo() {
-        return activo;
+        super(idTorneo, cedulaOrganizador, nombreTorneo, valorAmarilla, valorAzul, valorRoja, valorArbitraje, valorInscripcion, valorBalonPetos, partidosInicial, categoriaTorneo, activo);
     }
 }
